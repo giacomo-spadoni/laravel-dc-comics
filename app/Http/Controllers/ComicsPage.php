6e42e1c57d1c130162c83609d9899ad0f4b0ab0a
giacomo-spadoni <giaky.spado@gmail.com>
2024-07-05 12:41:49 +0200

    added empty form page in create()

--- app/Http/Controllers/ComicsPage.php
+++ app/Http/Controllers/ComicsPage.php
@@ -24,7 +24,7 @@ class ComicsPage extends Controller
      */
     public function create()
     {
-        //
+        return view('form');
     }
 
     /**
---
+++ resources/views/form.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <form action="">
+        <input type="text">
+        <input type="text">
+    </form>
+@endsection
